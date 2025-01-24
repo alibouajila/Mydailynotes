0c96399c315d9a29c908faa9c25ba01341c5a528
Ali Bouajila <alibouajila18@gmail.com>
2025-01-24 19:59:36 +0100

    App initiation

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express =require("express")
+const mongoose = require("mongoose")
+const cors=require("cors")
+
+const app=express()
+
+
+
+
+
+
+
+app.listen(3001,()=>{
+    console.log("Server is running ...")
+})
